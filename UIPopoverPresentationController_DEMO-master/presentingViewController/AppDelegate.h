cf796bf465c1af8498776a425554b4a723ebb28d
ericyang <363099697@qq.com>
2017-03-27 16:11:08 +0800

    .
    Signed-off-by: ericyang <363099697@qq.com>

---
+++ UIPopoverPresentationController_DEMO-master/presentingViewController/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  presentingViewController
+//
+//  Created by yiban on 15/8/17.
+//  Copyright (c) 2015年 yiban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
